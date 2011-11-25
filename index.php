778790553ae86ad34986d27b4a73f6a900b63c15
Ajay Sharma <aj@ajsharma.com>
2011-11-25 15:54:47 -0800

    Initial submission with css hovers and sticky footer

---
+++ index.php
@@ -0,0 +1,80 @@
+<!doctype html> 
+<html>
+<head>
+	<meta charset="utf-8">
+	<!-- Add "maximum-scale=1" to fix the weird iOS auto-zoom bug on orientation changes. -->
+	<meta name="viewport" content="width=device-width; initial-scale=1"/>  
+
+	<link rel="stylesheet" type="text/css" href="css/reset.css" />
+	<link rel="stylesheet" type="text/css" href="css/main.css" media="all" />
+	<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<link rel="shortcut icon" href="/images/favicon.ico"/> 
+
+	<title>Open Navigation Demo</title>	
+</head>
+<body>
+	<div class="global-wrapper">
+		<header class="global-header">
+			<nav class="global-navigation">
+				<ol>
+					<li><a href="#1">Item 1</a></li>
+					<li><a href="#2">Item 2</a></li>
+					<li>
+						<a href="#3">Item 3</a>
+						<ol>
+							<li><a href="#3a">Item 3a</a></li>
+							<li><a href="#3b">Item 3b</a></li>
+							<li><a href="#3c">Item 3c</a></li>
+						</ol>
+					</li>
+					<li><a href="#4">Item 4</a></li>
+					<li><a href="#5">Item 5</a></li>
+					<li>
+						<a href="#6">Item 6</a>
+						<ol>
+							<li><a href="#6a">Item 6a</a></li>
+							<li><a href="#6b">Item 6b</a></li>
+							<li><a href="#6c">Item 6c</a></li>
+						</ol>
+					</li>
+					<li>
+						<a href="#7">Item 7 has a longer name</a>
+						<ol>
+							<li><a href="#7a">Item 7a</a></li>
+							<li><a href="#7b">Item 7b</a></li>
+							<li><a href="#7c">Item 7c</a></li>
+						</ol>
+					</li>
+					<li>
+						<a href="#8">Item 8</a>
+						<ol>
+							<li><a href="#8a">Item 8a</a></li>
+							<li><a href="#8b">Item 8b</a></li>
+							<li><a href="#8c">Item 8c</a></li>
+						</ol>
+					</li>
+				</ol>
+			</nav>
+		</header>
+
+		<div class="global-content">
+			<section class="content">
+				<header class="content-header">
+					<h1>This is an example page</h1>
+				</header>
+				<p>Consectetur exercitation chambray american apparel aesthetic sed. Veniam excepteur ullamco, leggings put a bird on it DIY enim butcher fugiat beard letterpress. Lo-fi consectetur fugiat sint. Ea laborum mixtape banksy. Et DIY thundercats, consequat lomo non portland fugiat. Mustache terry richardson nihil cliche, non chambray scenester sint et. Art party american apparel skateboard gluten-free mixtape +1.<p>
+
+				<p>Lo-fi beard cliche thundercats stumptown jean shorts cred single-origin coffee, keffiyeh chambray dolor. Cupidatat stumptown craft beer, ad skateboard retro mcsweeney's helvetica occaecat in art party. Vero four loko assumenda, freegan sapiente locavore put a bird on it. Fixie 3 wolf moon put a bird on it, freegan leggings twee proident wes anderson aute mcsweeney's single-origin coffee. Viral leggings salvia odio retro delectus. Non portland officia master cleanse pitchfork, elit butcher culpa food truck. Locavore commodo leggings, butcher echo park accusamus iphone raw denim single-origin coffee.</p>
+			</section>
+
+		</div>
+		<div class=".global-footer-push"></div>
+	</div>
+	<footer class="global-footer">
+		<p>Hello I am a sticky footer!</p>
+	</footer>
+</body>
+</html>
